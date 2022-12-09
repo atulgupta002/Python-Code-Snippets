e4fdcf33eb60d38bfc9f43f9371c85914db82ccd
Atul Gupta <52090961+atulgupta002@users.noreply.github.com>
2022-12-09 03:20:25 -0500

    Square and sort a sorted list

---
+++ sorted_squares.py
@@ -0,0 +1,17 @@
+#Given an integer array nums sorted in non-decreasing order, return an array of the squares of each number sorted in non-decreasing order.
+
+def sorted_squares(nums):
+  p1 = 0
+  p2 = len(nums) - 1
+  result = []
+  for i in range(p2,-1,-1):
+    temp = 0
+    if abs(nums[p1]) < abs(nums[p2]):
+      temp = abs(nums[p2])
+      p2 -= 1
+    else:
+      temp = abs(nums[p1])
+      p1 += 1
+    result = [temp * temp] + result
+  return result
+      
