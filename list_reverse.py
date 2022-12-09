11157fd6b5c51e56f8ee10dc4bdca66566543c71
Atul Gupta <52090961+atulgupta002@users.noreply.github.com>
2022-12-09 00:22:19 -0500

    Create list_reverse.py

---
+++ list_reverse.py
@@ -0,0 +1,17 @@
+# Function to reverse a list in-place using O(1) memory. Demonstration of the two-pointer method. Time-complexity: O(n)
+
+def swap(a,b):
+  temp = a
+  a = b
+  b = temp
+  return
+
+def reverse(s):
+  assert isinstance(s,list)
+  p1 = 0
+  p2 = len(s)
+  while p1 < p2:
+    swap(s[p1],s[p2])
+    p1 += 1
+    p2 -= 1
+                 
